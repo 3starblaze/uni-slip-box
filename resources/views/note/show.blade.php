2e6bf810de0039b0c82c92f49aceb25551d193e7
Deniss Fjodorovs <3starblaze@gmail.com>
2020-10-18 07:14:42 +0300

    Add edit link in note.show

--- resources/views/note/show.blade.php
+++ resources/views/note/show.blade.php
@@ -4,4 +4,7 @@
     <h1>{{ $note->title }}</h1>
     <p>{{ $note->body }}</p>
     <p>By {{ $note->user->name }}</p>
+    @if (\Auth::user() == $note->user)
+        <p><a href="{{ route('note.edit', $note) }}">Edit</a></p>
+    @endif
 @endsection
