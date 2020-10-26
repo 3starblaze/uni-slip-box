d67ef04ecc80eb179aba96516f9879f365e8bc81
Deniss Fjodorovs <3starblaze@gmail.com>
2020-10-26 16:55:26 +0200

    Add edit tests

--- tests/Feature/NoteTest.php
+++ tests/Feature/NoteTest.php
@@ -5,7 +5,7 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
-use App\Models\Note;
+use App\Models\{Note, User};
 
 class NoteTest extends TestCase
 {
@@ -42,6 +42,43 @@ class NoteTest extends TestCase
     public function testForbidUnauthorizedNoteCreation()
     {
         $this->post(route('note.store'), (array) Note::factory()->make())
+             ->assertRedirect(route('home'));
+    }
+
+    /**
+     * Check if a guest is redirected when trying to edit a note
+     */
+    public function testForbidGuestEdit()
+    {
+        $note = Note::factory()->create();
+        $this->get(route('note.edit', $note))
+             ->assertRedirect(route('home'));
+    }
+
+    /**
+     * Check if authorized user is allowed (not redirected) to edit their note
+     */
+    public function testAllowAuthorizedEdit()
+    {
+        $user = User::factory()->create();
+        $note = Note::factory()->state([
+            'user_id' => $user->id,
+        ])->create();
+        $this->actingAs($user)
+             ->get(route('note.edit', $note))
+             ->assertStatus(200);
+    }
+
+    /**
+     * Check if unauthorized user is redirected after trying to edit a foreign
+     * note
+     */
+    public function testForbidUnauthorizedUserEdit()
+    {
+        $user = User::factory()->create();
+        $note = Note::factory()->create();
+        $this->actingAs($user)
+             ->get(route('note.edit', $note))
              ->assertRedirect(route('home'));
     }
 }
